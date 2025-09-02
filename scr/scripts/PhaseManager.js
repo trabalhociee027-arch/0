029c305e78a24f0fec8b2f0637efb9c70801f250
trabalhociee027-arch <trabalhociee027@gmail.com>
2025-09-02 13:37:47 -0300

    Update PhaseManager.js

--- scr/scripts/PhaseManager.js
+++ scr/scripts/PhaseManager.js
@@ -1,1 +1,15 @@
+export class PhaseManager {
+  constructor(phases) {
+    this.phases = phases;
+    this.currentPhase = 0;
+  }
 
+  nextPhase() {
+    if(this.currentPhase < this.phases.length - 1) {
+      this.currentPhase++;
+      console.log(`Fase atual: ${this.phases[this.currentPhase].name}`);
+    } else {
+      console.log("Jogo concluído!");
+    }
+  }
+}
