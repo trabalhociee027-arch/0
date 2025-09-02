8f6a7ea6965f8b47fa929ce2c99f1cd9b5117560
trabalhociee027-arch <trabalhociee027@gmail.com>
2025-09-02 13:37:08 -0300

    Update PlayerController.js

--- scr/scripts/PlayerController.js
+++ scr/scripts/PlayerController.js
@@ -1,1 +1,16 @@
+export class PlayerController {
+  constructor(name) {
+    this.name = name;
+    this.empatia = 0;
+    this.skills = [];
+  }
 
+  move(direction) {
+    console.log(`${this.name} se moveu para ${direction}`);
+  }
+
+  levelUpEmpatia() {
+    this.empatia += 1;
+    console.log(`${this.name} aumentou Empatia para ${this.empatia}`);
+  }
+}
